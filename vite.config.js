5788807b5267a43eb079de1b1939e634e31cceec
vighneshc21 <vighnesh.chaurasia@wowlabz.com>
2024-12-19 20:55:51 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve("src", "main.jsx"),
+      name: "react-lib-demo",
+      fileName: (format) => `react-lib-demo.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [react()],
+});
